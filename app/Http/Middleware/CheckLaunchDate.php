b7be2d8277042e50b505aac29a4642779ea02cbc
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-02-25 10:47:58 +0100

    Create CheckLaunchDate.php

---
+++ app/Http/Middleware/CheckLaunchDate.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckLaunchDate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Set the launch date
+        $launchDate = Carbon::create('2025-03-20');
+
+        // Check if the current date is after the launch date
+        if (now()->lessThan($launchDate)) {
+            return response()->view('coming-soon');
+        }
+
+        return $next($request);
+    }
+}
