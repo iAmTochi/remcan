7aaee3f2f66465eaa2e7d0889852a88e1bbcdc10
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-08 23:56:58 +0100

    Set up login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,4 @@
+@if(false)
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
@@ -45,3 +46,86 @@
         </div>
     </form>
 </x-guest-layout>
+@endif
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name') }} - Real Estate Marketers and Consultants Association of Nigeria</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <meta name="website" content="" />
+    <meta name="email" content="" />
+    <meta name="version" content="1.0.0" />
+    <!-- favicon -->
+    <link href="images/favicon.ico" rel="shortcut icon">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+    <!--Material Icon -->
+    <link href="css/materialdesignicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- Custom  Css -->
+    <link href="css/style.min.css" rel="stylesheet" type="text/css" id="theme-opt" />
+
+</head>
+
+<body>
+<!-- Start Hero -->
+<section class="bg-home zoom-image d-flex align-items-center">
+    <div class="bg-overlay image-wrap" style="background: url('images/bg/03.jpg') center;"></div>
+    <div class="bg-overlay bg-gradient-overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="p-4 bg-white rounded-3 shadow-md mx-auto w-100" style="max-width: 400px;">
+                    <form>
+                        <a href="index.html"><img src="{{asset('images/logo.png')}}" width="80" class="mb-4 d-block mx-auto" alt=""></a>
+                        <h5 class="mb-3">Please sign in</h5>
+
+                        <div class="form-floating mb-2">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                            <label for="floatingInput">Email address</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                            <label for="floatingPassword">Password</label>
+                        </div>
+
+                        <div class="d-flex justify-content-between">
+                            <div class="mb-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                    <label class="form-check-label text-muted" for="flexCheckDefault">Remember me</label>
+                                </div>
+                            </div>
+                            <span class="forgot-pass text-muted mb-0"><a href="auth-reset-password.html" class="text-muted">Forgot password ?</a></span>
+                        </div>
+
+                        <button class="btn btn-primary w-100" type="submit">Sign in</button>
+
+                        <div class="col-12 text-center mt-3">
+                            <span><span class="text-muted me-2">Don't have an account ?</span> <a href="auth-signup.html" class="text-dark fw-medium">Sign Up</a></span>
+                        </div><!--end col-->
+                    </form>
+                </div>
+            </div><!--end col-->
+        </div><!--end row-->
+    </div><!--end container-->
+</section><!--end section-->
+<!-- ENd Hero -->
+
+<!-- javascript -->
+<script src="js/bootstrap.bundle.min.js"></script>
+<!-- Icons -->
+<script src="js/feather.min.js"></script>
+<!-- Custom -->
+<script src="js/plugins.init.js"></script>
+<script src="js/app.js"></script>
+
+</body>
+
+</html>
