9348763af0b2ccf2f4fc0166283b60f538b66988
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-14 11:03:38 +0100

    Create blog-section.blade.php

---
+++ resources/views/components/blog-section.blade.php
@@ -0,0 +1,94 @@
+<div {{ $attributes->class(['container mt-100 mt-60']) }}>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="section-title text-center mb-4 pb-2">
+                <h4 class="title mb-3">Blogs or News</h4>
+                <p class="text-muted para-desc mb-0 mx-auto">A great plateform to buy, sell and rent your properties
+                    without any agent or commisions.</p>
+            </div>
+        </div><!--end col-->
+    </div><!--end row-->
+
+    <div class="row g-4 mt-0">
+        <div class="col-lg-4 col-md-6">
+            <div class="card blog blog-primary shadow rounded-3 overflow-hidden border-0">
+                <div class="card-img blog-image position-relative overflow-hidden rounded-0">
+                    <div class="position-relative overflow-hidden">
+                        <img src="images/property/1.jpg" class="img-fluid" alt="">
+                        <div class="card-overlay"></div>
+                    </div>
+
+                    <div class="blog-tag p-3">
+                        <a href="#" class="badge bg-primary">Towntor</a>
+                    </div>
+                </div>
+
+                <div class="card-body content p-0">
+                    <div class="p-4">
+                        <a href="blog-detail.html" class="title fw-medium fs-5 text-dark">Skills That You Can Learn In
+                            The Real Estate Market</a>
+                        <p class="text-muted mt-2">The most well-known dummy text is the 'Lorem Ipsum', in the 16th
+                            century.</p>
+
+                        <a href="#" class="text-dark read-more">Read More <i
+                                    class="mdi mdi-chevron-right align-middle"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div><!--end col-->
+
+        <div class="col-lg-4 col-md-6">
+            <div class="card blog blog-primary shadow rounded-3 overflow-hidden border-0">
+                <div class="card-img blog-image position-relative overflow-hidden rounded-0">
+                    <div class="position-relative overflow-hidden">
+                        <img src="images/property/2.jpg" class="img-fluid" alt="">
+                        <div class="card-overlay"></div>
+                    </div>
+
+                    <div class="blog-tag p-3">
+                        <a href="#" class="badge bg-primary">Towntor</a>
+                    </div>
+                </div>
+
+                <div class="card-body content p-0">
+                    <div class="p-4">
+                        <a href="blog-detail.html" class="title fw-medium fs-5 text-dark">Learn The Truth About Real
+                            Estate Industry</a>
+                        <p class="text-muted mt-2">The most well-known dummy text is the 'Lorem Ipsum', in the 16th
+                            century.</p>
+
+                        <a href="#" class="text-dark read-more">Read More <i
+                                    class="mdi mdi-chevron-right align-middle"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div><!--end col-->
+
+        <div class="col-lg-4 col-md-6">
+            <div class="card blog blog-primary shadow rounded-3 overflow-hidden border-0">
+                <div class="card-img blog-image position-relative overflow-hidden rounded-0">
+                    <div class="position-relative overflow-hidden">
+                        <img src="images/property/3.jpg" class="img-fluid" alt="">
+                        <div class="card-overlay"></div>
+                    </div>
+
+                    <div class="blog-tag p-3">
+                        <a href="#" class="badge bg-primary">Towntor</a>
+                    </div>
+                </div>
+
+                <div class="card-body content p-0">
+                    <div class="p-4">
+                        <a href="blog-detail.html" class="title fw-medium fs-5 text-dark">10 Quick Tips About Business
+                            Development</a>
+                        <p class="text-muted mt-2">The most well-known dummy text is the 'Lorem Ipsum', in the 16th
+                            century.</p>
+
+                        <a href="#" class="text-dark read-more">Read More <i
+                                    class="mdi mdi-chevron-right align-middle"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div><!--end col-->
+    </div><!--end row-->
+</div>
