7a07ef44b08c67381b5c2779a17a71576c21693f
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-14 11:03:52 +0100

    Create mission-vision.blade.php

---
+++ resources/views/components/mission-vision.blade.php
@@ -0,0 +1,41 @@
+<div {{ $attributes->class(['row g-4 mt-0']) }}>
+    <div class="col-md-6">
+        <div class="position-relative features text-center mx-lg-4 px-md-1">
+            <div class="feature-icon position-relative overflow-hidden d-flex justify-content-center">
+                <i data-feather="hexagon" class="hexagon"></i>
+                <div class="position-absolute top-50 start-50 translate-middle">
+                    <i data-feather="home" class="fea icon-m-md text-primary"></i>
+                </div>
+            </div>
+
+            <div class="mt-4">
+                <a href="#" class="fw-medium title text-dark fs-5">MISSION</a>
+                <p class="text-muted mt-3 mb-0">To empower and unite real estate marketers
+                    and consultants in Nigeria through advocacy, education, and
+                    professional development, promoting best practices, ethical standards, and collaborative
+                    opportunities that drive growth and success
+                    for our members and contribute to the nation's real estate sector.</p>
+            </div>
+        </div>
+    </div><!--end col-->
+
+    <div class="col-md-6">
+        <div class="position-relative features text-center mx-lg-4 px-md-1">
+            <div class="feature-icon position-relative overflow-hidden d-flex justify-content-center">
+                <i data-feather="hexagon" class="hexagon"></i>
+                <div class="position-absolute top-50 start-50 translate-middle">
+                    <i data-feather="briefcase" class="fea icon-m-md text-primary"></i>
+                </div>
+            </div>
+
+            <div class="mt-4">
+                <a href="#" class="fw-medium title text-dark fs-5">VISION</a>
+                <p class="text-muted mt-3 mb-0">To be the leading voice and standard-bearer for
+                    real estate marketers, Realtors, Brokers and consultants in Nigeria,
+                    fostering professionalism, innovation,
+                    and ethical practices to elevate the industry and enhance community trust.
+                </p>
+            </div>
+        </div>
+    </div><!--end col-->
+</div>
