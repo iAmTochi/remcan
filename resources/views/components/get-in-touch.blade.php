daa27a39919c933dd252b49d62100a1594785d2f
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-14 11:03:48 +0100

    Create get-in-touch.blade.php

---
+++ resources/views/components/get-in-touch.blade.php
@@ -0,0 +1,15 @@
+<div {{ $attributes->class(['container mt-100 mt-60']) }}>
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <div class="section-title text-center">
+                <h4 class="title mb-3">Have Question ? Get in touch!</h4>
+                <p class="text-muted para-desc mb-0 mx-auto">A great plateform to buy, sell and rent your properties
+                    without any agent or commisions.</p>
+                <div class="mt-4 pt-2">
+                    <a href="{{ route('contact-us') }}" class="btn btn-pills btn-primary"><span class="h5 mb-0 me-1"><i
+                                    data-feather="mail" class="fea icon-sm"></i></span> Contact us</a>
+                </div>
+            </div>
+        </div><!--end col-->
+    </div><!--end row-->
+</div>
