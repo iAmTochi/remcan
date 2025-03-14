50f89c3a2765462ba07eeb3e5ab28db500d7e211
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-14 21:58:58 +0100

    Create head-assets.blade.php

---
+++ resources/views/components/head-assets.blade.php
@@ -0,0 +1,12 @@
+<!-- favicon -->
+<link href="{{asset('images/favicon.ico')}}" rel="shortcut icon">
+<!-- Bootstrap core CSS -->
+<link href="{{asset('')}}/css/bootstrap.min.css" type="text/css" rel="stylesheet"/>
+<!-- Slider -->
+<link href="{{asset('')}}/css/swiper.min.css" rel="stylesheet"/>
+<!-- Tobii -->
+<link href="{{asset('')}}/css/tobii.min.css" rel="stylesheet" type="text/css"/>
+<!--Material Icon -->
+<link href="{{asset('')}}/css/materialdesignicons.min.css" rel="stylesheet" type="text/css"/>
+<!-- Custom  Css -->
+<link href="{{asset('')}}/css/style.min.css" rel="stylesheet" type="text/css" id="theme-opt"/>
