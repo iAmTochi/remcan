862b276a03d6822c2aa684907c8b9bfcbb5b3723
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-14 11:03:55 +0100

    Create teams.blade.php

---
+++ resources/views/components/teams.blade.php
@@ -0,0 +1,105 @@
+<div {{ $attributes->class(['container mt-100 mt-60']) }}>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="section-title text-center mb-4 pb-2">
+                <h4 class="title mb-3">Property Broker</h4>
+                <p class="text-muted para-desc mb-0 mx-auto">A great plateform to buy, sell and rent your properties
+                    without any agent or commisions.</p>
+            </div>
+        </div><!--end col-->
+    </div><!--end row-->
+
+    <div class="row g-4 mt-0">
+        <div class="col-lg-3 col-md-4 col-12">
+            <div class="card team team-primary text-center">
+                <div class="card-img team-image d-inline-block mx-auto rounded-pill avatar avatar-ex-large overflow-hidden">
+                    <img src="images/client/04.jpg" class="img-fluid" alt="">
+                    <div class="card-overlay avatar avatar-ex-large rounded-pill"></div>
+
+                    <ul class="list-unstyled team-social mb-0">
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="facebook" class="icons fea-social"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="instagram" class="icons fea-social"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="twitter" class="icons fea-social"></i></a></li>
+                    </ul><!--end icon-->
+                </div>
+
+                <div class="content mt-3">
+                    <a href="page-team-detail.html" class="text-dark h5 mb-0 title">Jack John</a>
+                    <h6 class="text-muted mb-0 fw-normal">Broker</h6>
+                </div>
+            </div>
+        </div><!--end col-->
+
+        <div class="col-lg-3 col-md-4 col-12">
+            <div class="card team team-primary text-center">
+                <div class="card-img team-image d-inline-block mx-auto rounded-pill avatar avatar-ex-large overflow-hidden">
+                    <img src="images/client/05.jpg" class="img-fluid" alt="">
+                    <div class="card-overlay avatar avatar-ex-large rounded-pill"></div>
+
+                    <ul class="list-unstyled team-social mb-0">
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="facebook" class="icons fea-social"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="instagram" class="icons fea-social"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="twitter" class="icons fea-social"></i></a></li>
+                    </ul><!--end icon-->
+                </div>
+
+                <div class="content mt-3">
+                    <a href="page-team-detail.html" class="text-dark h5 mb-0 title">Krista John</a>
+                    <h6 class="text-muted mb-0 fw-normal">Broker</h6>
+                </div>
+            </div>
+        </div><!--end col-->
+
+        <div class="col-lg-3 col-md-4 col-12">
+            <div class="card team team-primary text-center">
+                <div class="card-img team-image d-inline-block mx-auto rounded-pill avatar avatar-ex-large overflow-hidden">
+                    <img src="images/client/06.jpg" class="img-fluid" alt="">
+                    <div class="card-overlay avatar avatar-ex-large rounded-pill"></div>
+
+                    <ul class="list-unstyled team-social mb-0">
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="facebook" class="icons fea-social"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="instagram" class="icons fea-social"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="twitter" class="icons fea-social"></i></a></li>
+                    </ul><!--end icon-->
+                </div>
+
+                <div class="content mt-3">
+                    <a href="page-team-detail.html" class="text-dark h5 mb-0 title">Roger Jackson</a>
+                    <h6 class="text-muted mb-0 fw-normal">Broker</h6>
+                </div>
+            </div>
+        </div><!--end col-->
+
+        <div class="col-lg-3 col-md-4 col-12">
+            <div class="card team team-primary text-center">
+                <div class="card-img team-image d-inline-block mx-auto rounded-pill avatar avatar-ex-large overflow-hidden">
+                    <img src="images/client/07.jpg" class="img-fluid" alt="">
+                    <div class="card-overlay avatar avatar-ex-large rounded-pill"></div>
+
+                    <ul class="list-unstyled team-social mb-0">
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="facebook" class="icons fea-social"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="instagram" class="icons fea-social"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="btn btn-sm btn-pills btn-icon"><i
+                                        data-feather="twitter" class="icons fea-social"></i></a></li>
+                    </ul><!--end icon-->
+                </div>
+
+                <div class="content mt-3">
+                    <a href="page-team-detail.html" class="text-dark h5 mb-0 title">Johnny English</a>
+                    <h6 class="text-muted mb-0 fw-normal">Broker</h6>
+                </div>
+            </div>
+        </div><!--end col-->
+    </div><!--end row-->
+</div>
