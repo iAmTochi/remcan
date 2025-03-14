de9ad11129d52232f3eba51bca2f3b8babeeb51e
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-14 08:30:02 +0100

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -54,83 +54,20 @@
         <div id="navigation">
             <ul class="navigation-menu nav-left nav-light">
                 <li class=" {{ request()->routeIs('home') ? 'active':'' }} has-submenu parent-menu-item">
-                    <a href="{{ url('/') }}">Home</a>
+                    <a href="{{ route('home') }}">Home</a>
                 </li>
 
-                <li><a href="buy.html" class="sub-menu-item">Buy</a></li>
-
-                <li><a href="sell.html" class="sub-menu-item">Sell</a></li>
-
-                <li class="has-submenu parent-parent-menu-item">
-                    <a href="javascript:void(0)">Listing</a><span class="menu-arrow"></span>
-                    <ul class="submenu">
-                        <li class="has-submenu parent-menu-item"><a href="javascript:void(0)"> Grid View </a><span
-                                    class="submenu-arrow"></span>
-                            <ul class="submenu">
-                                <li><a href="grid.html" class="sub-menu-item">Grid Listing</a></li>
-                                <li><a href="grid-sidebar.html" class="sub-menu-item">Grid Sidebar </a></li>
-                            </ul>
-                        </li>
-                        <li class="has-submenu parent-menu-item"><a href="javascript:void(0)"> List View </a><span
-                                    class="submenu-arrow"></span>
-                            <ul class="submenu">
-                                <li><a href="list.html" class="sub-menu-item">List Listing</a></li>
-                                <li><a href="list-sidebar.html" class="sub-menu-item">List Sidebar </a></li>
-                            </ul>
-                        </li>
-                        <li class="has-submenu parent-menu-item"><a href="javascript:void(0)"> Property Detail </a><span
-                                    class="submenu-arrow"></span>
-                            <ul class="submenu">
-                                <li><a href="property-detail.html" class="sub-menu-item">Property Detail</a></li>
-                                <li><a href="property-detail-two.html" class="sub-menu-item">Property Detail Two</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
+                <li class=" {{ request()->routeIs('about') ? 'active':'' }}">
+                    <a href="{{ route('about') }}" class="sub-menu-item">About</a>
                 </li>
 
-                <li class="has-submenu parent-parent-menu-item">
-                    <a href="javascript:void(0)">Pages</a><span class="menu-arrow"></span>
-                    <ul class="submenu">
-                        <li><a href="aboutus.html" class="sub-menu-item">About Us</a></li>
-                        <li><a href="features.html" class="sub-menu-item">Features</a></li>
-                        <li><a href="pricing.html" class="sub-menu-item">Pricing</a></li>
-                        <li><a href="faqs.html" class="sub-menu-item">Faqs</a></li>
-                        <li class="has-submenu parent-menu-item"><a href="javascript:void(0)"> Auth Pages </a><span
-                                    class="submenu-arrow"></span>
-                            <ul class="submenu">
-                                <li><a href="auth-login.html" class="sub-menu-item">Login</a></li>
-                                <li><a href="auth-signup.html" class="sub-menu-item">Signup</a></li>
-                                <li><a href="auth-re-password.html" class="sub-menu-item">Reset Password</a></li>
-                            </ul>
-                        </li>
-                        <li class="has-submenu parent-menu-item"><a href="javascript:void(0)"> Utility </a><span
-                                    class="submenu-arrow"></span>
-                            <ul class="submenu">
-                                <li><a href="terms.html" class="sub-menu-item">Terms of Services</a></li>
-                                <li><a href="privacy.html" class="sub-menu-item">Privacy Policy</a></li>
-                            </ul>
-                        </li>
-                        <li class="has-submenu parent-menu-item"><a href="javascript:void(0)"> Blog </a><span
-                                    class="submenu-arrow"></span>
-                            <ul class="submenu">
-                                <li><a href="blogs.html" class="sub-menu-item"> Blogs</a></li>
-                                <li><a href="blog-sidebar.html" class="sub-menu-item"> Blog Sidebar</a></li>
-                                <li><a href="blog-detail.html" class="sub-menu-item"> Blog Detail</a></li>
-                            </ul>
-                        </li>
-                        <li class="has-submenu parent-menu-item"><a href="javascript:void(0)"> Special </a><span
-                                    class="submenu-arrow"></span>
-                            <ul class="submenu">
-                                <li><a href="coming-soon.blade.php" class="sub-menu-item">Comingsoon</a></li>
-                                <li><a href="maintenance.html" class="sub-menu-item">Maintenance</a></li>
-                                <li><a href="error.html" class="sub-menu-item">404! Error</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+                <li class=" {{ request()->routeIs('services') ? 'active':'' }}">
+                    <a href="{{ route('services') }}" class="sub-menu-item">Services</a>
                 </li>
 
-                <li><a href="contactus.html" class="sub-menu-item">Contact Us</a></li>
+                <li class=" {{ request()->routeIs('contact-us') ? 'active':'' }}">
+                    <a href="{{ route('contact-us') }}" class="sub-menu-item">Contact Us</a>
+                </li>
             </ul>
         </div>
     </div>
