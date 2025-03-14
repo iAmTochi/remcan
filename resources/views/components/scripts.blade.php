d1f5e839d42aa9e4380bdee87fb2d978d4885346
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-14 21:59:01 +0100

    Create scripts.blade.php

---
+++ resources/views/components/scripts.blade.php
@@ -0,0 +1,10 @@
+<script src="{{ asset('') }}js/bootstrap.bundle.min.js" {{ $attributes }}></script>
+<!-- Tiny slider -->
+<script src="{{ asset('') }}js/swiper.min.js"></script>
+<!-- Tobii -->
+<script src="{{ asset('') }}js/tobii.min.js"></script>
+<!-- Icons -->
+<script src="{{ asset('') }}js/feather.min.js"></script>
+<!-- Custom -->
+<script src="{{ asset('') }}js/plugins.init.js"></script>
+<script src="{{ asset('') }}js/app.js"></script>
