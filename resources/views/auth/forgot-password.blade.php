091f3bdd486812d39515bfcb60f7fe0c21ae8843
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-22 23:23:53 +0100

    Update forgot-password.blade.php

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <form>
+    <form action="{{ route('password.email') }}" method="post">
         @csrf
         <a href="{{ route('home') }}"><img src="{{asset('images/logo.png')}}" width="80" class="mb-4 d-block mx-auto" alt=""></a>
         <h5 class="mb-3">Reset your password</h5>
@@ -10,8 +10,11 @@
         <p class="text-muted">Please enter your email address. You will receive a link to create a new password via email.</p>
 
         <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
+            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email"  placeholder="name@remcanline.com">
+            <label for="email">Email address</label>
+            <span role="alert" class="invalid-feedback">
+                <strong>{{$errors->first('email')}}</strong>
+            </span>
         </div>
 
         <button class="btn btn-primary w-100" type="submit">Send</button>
