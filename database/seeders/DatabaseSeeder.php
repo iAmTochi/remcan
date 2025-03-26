1d8f06ac2ddb005886f32a3ced5567d9f6c7331e
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-26 09:38:46 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         User::factory(10)->create();
+         User::factory(400)->create();
 
         User::factory()->create([
             'first_name' => "Tochukwu",
