a6989c030048ad72d525cbcedcb25df6a96667d5
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-26 02:15:04 +0100

    Update 2025_03_24_113038_create_users_table.php

--- database/migrations/2025_03_24_113038_create_users_table.php
+++ database/migrations/2025_03_24_113038_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('type')->default('associate');
+            $table->string('type')->default('agent');
             $table->string('surname');
             $table->string('first_name');
             $table->string('other_names')->nullable();
