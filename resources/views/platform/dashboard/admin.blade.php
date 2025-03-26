372bb3df87ca249b28badc8475e34757d1fd6537
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-26 02:23:36 +0100

    Update admin.blade.php

--- resources/views/platform/dashboard/admin.blade.php
+++ resources/views/platform/dashboard/admin.blade.php
@@ -54,20 +54,20 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col mt-0">
-                                        <h5 class="card-title">Income</h5>
+                                        <h5 class="card-title">No of Agents</h5>
                                     </div>
 
                                     <div class="col-auto">
                                         <div class="avatar">
                                             <div class="avatar-title rounded-circle bg-primary-dark">
-                                                <i class="align-middle" data-feather="dollar-sign"></i>
+                                                <i class="align-middle" data-feather="users"></i>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <h1 class="display-5 mt-1 mb-3">$25.300</h1>
+                                <h1 class="display-5 mt-1 mb-3">0</h1>
                                 <div class="mb-0">
-                                    <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -2.65% </span>
+{{--                                    <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -2.65% </span>--}}
                                     Less sales than usual
                                 </div>
                             </div>
@@ -78,20 +78,20 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col mt-0">
-                                        <h5 class="card-title">Orders</h5>
+                                        <h5 class="card-title">Male</h5>
                                     </div>
 
                                     <div class="col-auto">
                                         <div class="avatar">
                                             <div class="avatar-title rounded-circle bg-primary-dark">
-                                                <i class="align-middle" data-feather="shopping-cart"></i>
+                                                <i class="align-middle" data-feather="users"></i>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <h1 class="display-5 mt-1 mb-3">12.514</h1>
+                                <h1 class="display-5 mt-1 mb-3">0</h1>
                                 <div class="mb-0">
-                                    <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 5.50% </span>
+{{--                                    <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 5.50% </span>--}}
                                     More visitors than usual
                                 </div>
                             </div>
@@ -102,20 +102,20 @@
                             <div class="card-body">
                                 <div class="row">
                                     <div class="col mt-0">
-                                        <h5 class="card-title">Activity</h5>
+                                        <h5 class="card-title">Female</h5>
                                     </div>
 
                                     <div class="col-auto">
                                         <div class="avatar">
                                             <div class="avatar-title rounded-circle bg-primary-dark">
-                                                <i class="align-middle" data-feather="activity"></i>
+                                                <i class="align-middle" data-feather="female"></i>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <h1 class="display-5 mt-1 mb-3">29.232</h1>
+                                <h1 class="display-5 mt-1 mb-3">0</h1>
                                 <div class="mb-0">
-                                    <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -4.25% </span>
+{{--                                    <span class="text-danger"> <i class="mdi mdi-arrow-bottom-right"></i> -4.25% </span>--}}
                                     More earnings than usual
                                 </div>
                             </div>
@@ -132,14 +132,14 @@
                                     <div class="col-auto">
                                         <div class="avatar">
                                             <div class="avatar-title rounded-circle bg-primary-dark">
-                                                <i class="align-middle" data-feather="dollar-sign"></i>
+                                                <i class="align-middle" data-feather="card"></i>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <h1 class="display-5 mt-1 mb-3">$83.300</h1>
+                                <h1 class="display-5 mt-1 mb-3">₦83.300</h1>
                                 <div class="mb-0">
-                                    <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 8.35% </span>
+{{--                                    <span class="text-success"> <i class="mdi mdi-arrow-bottom-right"></i> 8.35% </span>--}}
                                     More earnings than usual
                                 </div>
                             </div>
