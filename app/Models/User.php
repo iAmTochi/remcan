a4a5558a4c387279db0b963eab2c1235d265c281
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-29 23:02:22 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,12 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
+use phpDocumentor\Reflection\Types\Boolean;
 
 class User extends Authenticatable
 {
@@ -16,7 +18,7 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     protected $guarded = [];
-    
+
 
     /**
      * The attributes that should be hidden for serialization.
@@ -64,5 +66,20 @@ class User extends Authenticatable
     public function age(): Attribute
     {
         return Attribute::get(fn() => is_null($this->dob) ? '' : Carbon::parse($this->dob)->age);
+    }
+
+    public function category(): BelongsTo
+    {
+        return  $this->belongsTo(Category::class);
+    }
+
+    public function isMale():bool
+    {
+        return $this->gender == 'M';
+    }
+
+    public function isFemale():bool
+    {
+        return $this->gender == 'F';
     }
 }
