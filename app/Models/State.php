ebbe605a3946cf52097e5a162d159c3c6d8a0fa6
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-03-29 23:02:19 +0100

    Update State.php

--- app/Models/State.php
+++ app/Models/State.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class state extends Model
+class State extends Model
 {
-    
+
     public function lga(): HasMany
     {
         return $this->hasMany(Lga::class);
