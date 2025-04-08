632a9f4d37400ff873c00560c4d6c7670a4e9d62
Tochukwu <ugwukelvintochukwu@gmail.com>
2025-04-08 08:41:56 +0100

    Add SEO tags

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name') }} - Real Estate Marketers and Consultants Association of Nigeria</title>
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
+    <meta name="description" content="Join REMCAN – the biggest, best, and fastest-growing real estate association in Nigeria. Connect with top realtors, brokers, and consultants. Explore Nigeria’s real estate market trends and insights.">
+    <meta name="keywords" content="Biggest real estate association in Nigeria, Best real estate association in Nigeria, Fastest-growing real estate network in Nigeria, REMCAN – Nigeria’s leading real estate association, Real estate market trends in Nigeria, Nigeria’s real estate industry insights, Real estate professionals network in Nigeria, Join Nigeria’s top real estate association, REMCAN – Nigeria’s #1 Real Estate Association, REMCAN for Realtors, Brokers & Consultants in Nigeria">
+    <meta name="robots" content="index, follow">
     <meta name="author" content="Dynaton Technologies" />
     <meta name="website" content="" />
     <meta name="email" content="" />
